e21caefeab216706658440694a33c9325c85fa7f
Shaurya <shaurya.shauryagoyal123@gmail.com>
2026-05-30 02:39:38 +0530

    ops: prepare for deployment

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,3 +142,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,2 +1,5 @@
 from .base import *
+import os
+
 DEBUG = False
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
