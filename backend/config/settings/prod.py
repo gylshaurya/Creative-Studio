9e198269edffbbc07a1d7f61836a028f8e71171a
Shaurya <shaurya.shauryagoyal123@gmail.com>
2026-05-30 14:21:55 +0530

    fix: update CORS 2

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -4,6 +4,5 @@ import os
 DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 CORS_ALLOWED_ORIGINS = [
-    'https://creative-studio-phi.vercel.app/',
-    'https://creative-studio-production-01bd.up.railway.app/'
+    'https://creative-studio-phi.vercel.app'
 ]
